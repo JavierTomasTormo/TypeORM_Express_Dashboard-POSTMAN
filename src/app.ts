2a41fae26136b8e827e6d7d8901f95a451ff24dc
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2024-10-31 17:40:41 +0100

    RutaCambiada & puerto dotenv

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,16 @@ import express from 'express';
 import { connectDatabase } from './config/database';
 import postRoutes from './Post/Post.routes';
 import userAdminRoutes from './userAdmin/userAdmin.routes';
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use('/posts', postRoutes);
-app.use('/users', userAdminRoutes);
+app.use('/userAdmin', userAdminRoutes);
 
 connectDatabase().then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
